706b91a5e4133bef9e938d261fe2f4e24e947ca8
Parakh Gupta <89535374+parakh2001@users.noreply.github.com>
2023-09-26 20:26:44 +0530

    uploaded files from system.

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -g fish_greeting
+    set -x CHROME_EXECUTABLE /usr/bin/google-chrome-stable
+
+end
+motivate
